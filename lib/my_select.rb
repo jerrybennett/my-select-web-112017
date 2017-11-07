9aee000ed47b3ff6e7535d4a988145e267d88b89
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-07 20:12:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,7 +8,7 @@ def my_select(collection)
     #   end
     # end
     while i < collection.length
-      if collection[i] == true
+      if i == true
         new_collection << yield(collection[i])
       end
       i += 1
