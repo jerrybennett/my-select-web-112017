4ad10b7356188ccece21c59a293a7dfd5758287e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-07 20:09:43 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  # i = 0
+  if block_given?
+    # for e in collection
+    #   if e == true
+    #     new_collection.push yield e
+    #   end
+    # end
+    while i < collection.length
+      if collection[i] == true
+        new_collection << yield(collection[i])
+      end
+      i += 1
+    end
+  end
+  return new_collection
 end
